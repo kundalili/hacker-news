0193b59d5fc3c8ab1e8725328a123fbd73efa20c
Shir Koren <shirko6@gmail.com>
2022-09-17 17:59:57 +0200

    author-page and spinner

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Search from './component/Search'
 import { useState, useEffect } from 'react'
 import {useParams, Link} from "react-router-dom"
+import Spinner from './component/Spinner';
 function App() {
 
   const [userInput, setUserInput] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+  const [isLoading , setLoading] = useState(true)
   const [news, setNews] = useState([])
   const author = useParams()
 
@@ -17,9 +19,13 @@ function App() {
         
         const data = await result.json()
         console.log("🚀 ~ data", data.hits[0].title)
-  
-        setNews(data.hits)
         
+        setNews(data.hits)
+        setLoading(true)
+        setTimeout(()=>{
+          setLoading(false)
+        }, 2000)
+       
       } catch (error) {
         console.log('This is a wrong path')
         
@@ -27,11 +33,18 @@ function App() {
     }
     getData()
   },[searchQuery])
+ 
+  //we are trying to find the logic for the spinner, right now it's working not connected to the click
+    if (isLoading){
+      return <Spinner />
+    }
+  
 
   const onClick = (e) => {
     e.preventDefault();
     setSearchQuery(userInput)
-    console.log('')
+    console.log('') 
+    
 }
 
 const onChange = (e) => {
@@ -39,13 +52,19 @@ const onChange = (e) => {
 }
 
 
+
   return (
     <div>
      <Search value={userInput} onChange={onChange} onClick={onClick}/>
+    
+   
+        
       {news.map((item, key) => <div key={key} className="hack-item">
+        
         <Link className="hack-title">{item.title}</Link>
-        <Link to={`/author/${item.author}`} className="hack-author">{item.author}</Link>
+        <Link to={`/author/${item.author}`}  authorData={item.author}className="hack-author">{item.author}</Link>
         </div>)}
+
     </div>
   );
 }
