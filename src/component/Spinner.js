60790db434f92e8d2c17ac0501731a9611a507af
Shir Koren <shirko6@gmail.com>
2022-09-24 13:43:06 +0200

    spinner

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../App.css'
 
-export default function Spinner(props) {
-  return (
-    <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  )
-}
+export default function Spinner() {
+
+    return  (
+      <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+   
+
+  }
+  
+ 
+
